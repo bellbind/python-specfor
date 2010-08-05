d7320a7cfbc335a0bce10238e4e329e6ed673722
bellbind <bellbind@gmail.com>
2010-08-05 23:52:23 +0900

    [add] help for command

--- specfor/doc.py
+++ specfor/doc.py
@@ -6,13 +6,16 @@ from .docs import plugins
 
 def main():
     parser = optparse.OptionParser(
-        usage="usage: %prog [opts] module.name ...")
+        usage="\n".join(
+            ["%prog [opts] module.name ...",
+             "",
+             "Generate documents from specfor spec defined modules."]))
     parser.add_option(
         "-n", "--name", dest="name", default="simple",
         help="document generator name (default: %default)")
     parser.add_option(
         "-x", "--ext", dest="ext", default="md",
-        help="file ext (default: %default)")
+        help="generated file ext (default: %default)")
     opts, modnames = parser.parse_args()
     
     for modname in modnames:
