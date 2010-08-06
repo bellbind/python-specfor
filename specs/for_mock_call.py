c99f1822a555cbce005d73e1f24c349dd9c082ba
bellbind <bellbind@gmail.com>
2010-08-07 03:26:53 +0900

    [add] spec for count restriction

--- specs/for_mock_call.py
+++ specs/for_mock_call.py
@@ -5,8 +5,85 @@ count_mock_spec = spec.of("mock with count call")
 def behavior(its):
     from specfor import mock
     
+    counter_mock = mock.define("counter")
+    method_def = counter_mock.method("count")
+    @method_def.define(at=1)
+    def count(self):
+        return 1
+    
+    counter = counter_mock("id-0")
+    with the.raising[AssertionError]: mock.check(counter)
+    
+    counter.count()
+    mock.check(counter)
+    
+    counter.count()
+    with the.raising[AssertionError]: mock.check(counter)
     pass
 
-# TODO: spec for just, until, over, between
+@count_mock_spec.that("count over")
+def behavior(its):
+    from specfor import mock
+    
+    counter_mock = mock.define("counter")
+    method_def = counter_mock.method("count")
+    @method_def.define(over=1)
+    def count(self):
+        return 1
+    
+    counter = counter_mock("id-0")
+    with the.raising[AssertionError]: mock.check(counter)
+    
+    counter.count()
+    mock.check(counter)
+    
+    counter.count()
+    mock.check(counter)
+    pass
+
+@count_mock_spec.that("count until")
+def behavior(its):
+    from specfor import mock
+    
+    counter_mock = mock.define("counter")
+    method_def = counter_mock.method("count")
+    @method_def.define(until=1)
+    def count(self):
+        return 1
+    
+    counter = counter_mock("id-0")
+    mock.check(counter)
+    
+    counter.count()
+    mock.check(counter)
+    
+    counter.count()
+    with the.raising[AssertionError]: mock.check(counter)
+    pass
+
+@count_mock_spec.that("count between")
+def behavior(its):
+    from specfor import mock
+    
+    counter_mock = mock.define("counter")
+    method_def = counter_mock.method("count")
+    @method_def.define(between=(1, 3))
+    def count(self):
+        return 1
+    
+    counter = counter_mock("id-0")
+    with the.raising[AssertionError]: mock.check(counter)
+    
+    counter.count()
+    mock.check(counter)
+    counter.count()
+    mock.check(counter)
+    counter.count()
+    mock.check(counter)
+    
+    counter.count()
+    with the.raising[AssertionError]: mock.check(counter)
+    pass
+
 
 spec.publish(globals())
